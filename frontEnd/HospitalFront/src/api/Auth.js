612c26cc8c716ef74b8f9c3bd3a6b7a667870abb
DarioCastel <lecastelnovo@gmail.com>
2024-04-05 19:58:23 -0300

    loginValidation

--- frontEnd/HospitalFront/src/api/Auth.js
+++ frontEnd/HospitalFront/src/api/Auth.js
@@ -2,7 +2,5 @@ import axios from 'axios'
 
 const API = "http://localhost:3001/api"
 
-export const userLogin = data => {
-    console.log(data)
-    axios.post(`${API}/iniciar`,data)}
+export const userLogin = data => axios.post(`${API}/iniciar`,data)
 
--- frontEnd/HospitalFront/src/views/page/Login.jsx
+++ frontEnd/HospitalFront/src/views/page/Login.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useForm} from 'react-hook-form'
 import {userLogin} from "../../api/Auth"
 
 const Login = () => {
 
-  const {register,handleSubmit}=useForm()
+  const [errorMsj, setErrorMsj] = useState("")
+  const {register,handleSubmit,}=useForm();
+
+  useEffect(() => {
+    const setTime =setTimeout(()=>{
+      setErrorMsj("")
+    },3000)
+     return(
+      ()=>clearTimeout(setTime)
+    )
+  }, [errorMsj])
+  
+
+  const onSubmit = handleSubmit(async(data)=>{
+    try {
+      const res = await userLogin(data)
+      console.log(res)
+    } catch (error) {
+      setErrorMsj(error.response.data.mensaje)
+    }
+  })
+
+
+
   return (
     <>
-    <form onSubmit={handleSubmit(async(value)=>{
-      const res = await userLogin(value)
-      console.log(res)
-    })}>
-      <input type="text" {...register("Email",{required:true})}/>
-      <input type="password" {...register("Password",{required:true})}/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("email",{required:true})}/>
+      <input type="password" {...register("contraseña",{required:true})}/>
       <button>Sign in</button>
+      {errorMsj&&<span>{errorMsj}</span>}
     </form>
     </>
   )
