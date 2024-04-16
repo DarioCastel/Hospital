c317b70ef811c1e7d53af44d78704751117af734
DarioCastel <lecastelnovo@gmail.com>
2024-04-16 20:01:30 -0300

    calendarioV1

---
+++ frontEnd/HospitalFront/src/views/components/CalendarCon.jsx
@@ -0,0 +1,63 @@
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import dayjs from "dayjs";
+import 'dayjs/locale/es'
+
+
+dayjs.locale('es')
+
+
+const CalendarCon = () => {
+  const localizer = dayjsLocalizer(dayjs);
+
+
+  const event = [
+    {
+      start: dayjs("2024-04-11T14:00:00").toDate(),
+      end: dayjs("2024-04-11T15:00:00").toDate(),
+      title: "Turno 1",
+    },
+    {
+      start: dayjs("2024-04-11T17:00:00").toDate(),
+      end: dayjs("2024-04-11T18:00:00").toDate(),
+      title: "Turno 2",
+    },
+    {
+      start: dayjs("2024-04-15T14:00:00").toDate(),
+      end: dayjs("2024-04-15T15:00:00").toDate(),
+      title: "Turno 3",
+    },
+    {
+      start: dayjs("2024-04-18T12:00:00").toDate(),
+      end: dayjs("2024-04-18T13:00:00").toDate(),
+      title: "Turno 4",
+    },
+  ];
+
+
+  return (
+    <>
+      <Calendar
+        localizer={localizer}
+        events={event}
+        messages={{
+          allDay: "Todo el día",
+          previous: "Anterior",
+          next: "Siguiente",
+          today: "Hoy",
+          month: "Mes",
+          week: "Semana",
+          day: "Día",
+          agenda: "Agenda",
+          date: "Fecha",
+          time: "Hora",
+          event: "Evento",
+          noEventsInRange: "Sin eventos",
+        }}
+      />
+    </>
+  );
+};
+
+
+export default CalendarCon;
--- frontEnd/HospitalFront/src/views/page/Calendario.jsx
+++ frontEnd/HospitalFront/src/views/page/Calendario.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import CalendarCon from "../components/CalendarCon";
+import "../../css/calendario.css"
 
 const Calendario = () => {
   return (
-    <div>Calendario</div>
-  )
-}
+    <>
+      <div className="calendarioContainer">
+        <div className="contentCalendar">
+          <CalendarCon />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Calendario
+export default Calendario;
