4b8241f566cf97418da74743d22a4dc5f53564aa
DarioCastel <lecastelnovo@gmail.com>
2024-05-08 19:50:25 -0300

    agregarTurnoV1

--- frontEnd/HospitalFront/src/views/page/Turnos.jsx
+++ frontEnd/HospitalFront/src/views/page/Turnos.jsx
@@ -1,11 +1,23 @@
 
 import TableTurn from '../components/TableTurn'
 import "../../css/turno.css"
+import { useState } from 'react'
+import TurnoForm from "../components/TurnoForm"
 
 const Turnos = () => {
+
+  const [agregarBool, setAgregarBool] = useState(false)
+  function handleBtn() {
+    setAgregarBool(true)
+  }
+  const handleCancelar = () => {
+    setAgregarBool(false);
+  };
   return (
     <>
     <div className="containerTurnos">
+        <button className='btnTurno' onClick={handleBtn}>Agregar turno</button>
+        {agregarBool && <TurnoForm onCancel={handleCancelar}/>}
         <TableTurn/>
     </div>
     </>
